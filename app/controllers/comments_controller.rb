cb23d1044a36e50e2a08afd6ee7a7ab459f536df
Sai Kiran Kattamanchi <saikirankattamanchi@gmail.com>
2016-12-05 04:45:40 -0800

    First Commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.build(comment_params)
+    @comment.save
+    redirect_to @post
+  end
+
+  def edit
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.find params[:id]
+  end
+
+  def update
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.find params[:id]
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    else
+      render @post
+    end
+  end
+
+  def destroy
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.find params[:id]
+    @comment.destroy
+    redirect_to @post
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit!
+  end
+
+end
